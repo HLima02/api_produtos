6278f9655eaedbf366e2995556024e727499cd7b
HLima02 <hiago.liima02@hotmail.com>
2025-06-06 12:42:31 -0300

    Correção estrutura inicial api

--- api/src/server.js
+++ api/src/server.js
@@ -4,6 +4,14 @@ const app = express()
 app.use(express.json())
 const PORT = process.env.PORT || 500
 
+const router = express.Router()
+
+//rota de listagem de produtos
+router.get("/", async (req, res) => {
+
+})
+
+
 app.listen(PORT, () => {
   console.log("Rodando na porta 5000")
 })
